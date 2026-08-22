bdee56c42d2c4969424b72e845a849fff5e8849e
akihololive <torihikiaka@gmail.com>
2026-08-22 09:05:31 +0900

    Refactor app.py for improved functionality and updates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title="パチスロ 差枚チェッカー", page_icon="🎰", layout="wide")
 st.title("🎰 パチスロ：差枚チェッカー")
-st.markdown('直近の差枚数確認用、高設定が据えてあるわけじゃないよ！<span style="color:red;">※8月21日 更新</span>🐰', unsafe_allow_html=True)
+st.markdown('直近の差枚数確認用、高設定が据えてあるわけじゃないよ！<span style="color:red;">※8月22日 9:00 更新</span>🐰', unsafe_allow_html=True)
 
 GITHUB_USER = "akihololive"
 GITHUB_REPO = "pachislot-analysis"
